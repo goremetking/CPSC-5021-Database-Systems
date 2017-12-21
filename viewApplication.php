de55abb9e628d0707cc1221a754f62b0857ab66e
mkung <tarrant6@gmail.com>
2017-12-20 22:59:41 -0800

    Update viewApplication.php

--- viewApplication.php
+++ viewApplication.php
@@ -5,10 +5,10 @@
 </head>
 
 <?php
-    $servername = "cssql.seattleu.edu";
-    $username = "lind2";
-    $password = "4EKud*pu";
-    $dbname = "lind2";
+    $servername = "";
+    $username = "";
+    $password = "";
+    $dbname = "";
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
