40cb45624e95d408f3d76bffabd070ea1b92b896
mkung <tarrant6@gmail.com>
2017-12-20 22:55:43 -0800

    Update applicationInformation.php

--- applicationInformation.php
+++ applicationInformation.php
@@ -5,9 +5,9 @@
 
 	<?php
 		$servername = "cssql.seattleu.edu";
-		$username = "lind2";
-		$password = "4EKud*pu";
-		$dbname = "lind2";
+		$username = "";
+		$password = "";
+		$dbname = "";
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
 		if (!$conn) {
 			die("Connection failed: " . mysqli_connect_error());
